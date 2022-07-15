15bf6d4c31e750eb8d1e98a953914e866d54f96f
jac2206 <arango773@gmail.com>
2022-07-14 22:26:14 -0500

    Se quitaron comentarios

--- src/Infraestructura/ProductosService.js
+++ src/Infraestructura/ProductosService.js
@@ -22,50 +22,6 @@ function ObtenerDetalleProducto(category, article, description){
     return {categories, item}
 }
 
-// function ObtenerProductos(result){
-//     let items = [];
-//     result.slice(0,4)
-//         .map(item => {
-//             items.push({
-//             id: item.id,
-//             title: item.title,
-//             price:{
-//                 currency: item.currency_id,
-//                 amount: item.available_quantity,
-//                  decimals: item.price
-//                 },
-//             picture: item.thumbnail,
-//             condition: item.condition,
-//             free_shipping: (item.shipping && item.shipping.free_shipping === true),
-//             location:{
-//                     state: item.address.state_name,
-//                     city: item.address.city_name
-//                 }
-//             })
-//         });
-//     return items
-// }
-
-// function ObtenerDetalleProducto(category, article, description){
-//     let categories = (category.path_from_root || [])
-//     .map(path => path.name);
-//     let item = {
-//         id: article.id,
-//         title: article.title,
-//         price: {
-//             currency: article.currency_id,
-//             amount: article.available_quantity,
-//             decimals: article.price
-//         },
-//         picture: article.pictures && article.pictures.length > 0 ? article.pictures[0].url : article.thumbnail,
-//         condition: article.condition,
-//         free_shipping: (article.shipping && article.shipping.free_shipping === true),
-//         sold_quantity: article.sold_quantity,
-//         description: description.plain_text
-//     };
-//     return {categories, item}
-// }
-
 export { ObtenerCategorias, ObtenerProductos, ObtenerDetalleProducto };
 
 
