53ca658dd5d17cea8d203a2996fb52bb87d6dc8d
jac2206 <arango773@gmail.com>
2022-07-17 16:51:49 -0500

    Se creo archivo configuracion.js

--- src/app.js
+++ src/app.js
@@ -6,34 +6,11 @@ import productosController from "./Controllers/ProductosController.js";
 import cors from "cors"
 import swaggerJsDoc from "swagger-jsdoc"
 import swaggerUI from 'swagger-ui-express'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import {swaggerOptions} from './Common/Configuracion.js'
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000)
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const swaggerOptions = {
-    swaggerDefinition:{
-        openapi : '3.0.0',
-        info:{
-            title: "Productis MELI API",
-            version: '1.0.0',
-        },
-        servers:[
-                {
-                    url: "http://localhost:3000"
-                },
-                {
-                    url: "http://tiendaproductosmlapi.azurewebsites.net"
-                }
-        ],
-    },
-    apis: [`${path.join(__dirname, './Controllers/*.js')}`]
-};
 
 app.use(cors());
 app.use(morgan("common"));
